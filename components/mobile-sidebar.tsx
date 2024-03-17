7cbf08809920930fd16b74a3f1ba64b7c27c133a
Raymond Cancino <cancinoray@gmail.com>
2024-03-17 23:11:42 +0800

    add: sidebar for desktop and mobile view

---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,23 @@
+"use clients";
+
+import { Button } from "@/components/ui/button";
+
+import { Menu } from "lucide-react";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+
+const MobileSidebar = () => {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="p-0">
+
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileSidebar;
